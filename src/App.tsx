0bd16c49205af8d1efc7f84bfd8c841095258a3e
Thiago Arantes <thiagoarantes@gmail.com>
2023-05-06 17:14:55 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { styled } from "@stitches/react";
+
+const OuterContainer = styled("div", {
+  height: "100%",
+  width: "100%",
+  backgroundColor: "grey",
+});
+
+function App() {
+  return <OuterContainer>Future json2Tree App (WIP)</OuterContainer>;
+}
+
+export default App;
