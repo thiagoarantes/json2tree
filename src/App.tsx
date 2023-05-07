b7dd63179294fe98e5d49140be84d779945bba70
Thiago Arantes <thiagoarantes@gmail.com>
2023-05-06 23:18:36 -0400

    Reset File / Outer Container Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import { styled } from "@/stitches";
+import { globalCss, reset, styled } from "@/stitches";
+
+const globalStyles = globalCss(reset);
 
 const OuterContainer = styled("div", {
   height: "100%",
   width: "100%",
-  backgroundColor: "grey",
+  display: "flex",
+  padding: "$2",
+  gap: "$2",
+});
+
+const TreeViewContainer = styled("div", {
+  width: "400px",
+  backgroundColor: "$gray500",
+});
+
+const JSONContainer = styled("div", {
+  flexGrow: 1,
+  backgroundColor: "$gray400",
 });
 
 function App() {
-  return <OuterContainer>Future json2Tree App (WIP)</OuterContainer>;
+  globalStyles();
+
+  return (
+    <OuterContainer>
+      <TreeViewContainer>TreeView</TreeViewContainer>
+      <JSONContainer>JSON</JSONContainer>
+    </OuterContainer>
+  );
 }
 
 export default App;
--- src/stitches/stitches.config.ts
+++ src/stitches/stitches.config.ts
@@ -15,14 +15,47 @@ export const {
       gray400: "gainsboro",
       gray500: "lightgray",
     },
+    space: {
+      "0-5": "4px",
+      1: "8px",
+      "1-5": "12px",
+      2: "16px",
+      3: "24px",
+      4: "32px",
+    },
   },
   media: {
     bp1: "(min-width: 480px)",
   },
   utils: {
-    marginX: (value: string | number) => ({
+    m: (value: string | number) => ({
+      marginLeft: value,
+      marginRight: value,
+      marginTop: value,
+      marginBottom: value,
+    }),
+    mx: (value: string | number) => ({
       marginLeft: value,
       marginRight: value,
     }),
+    my: (value: string | number) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
   },
 });
+
+/**
+ * Reset CSS file
+ */
+export const reset = {
+  "*": {
+    boxSizing: "border-box",
+    margin: "0",
+    padding: "0",
+  },
+  "html, body, #root": {
+    fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+    height: "100%",
+  },
+};
