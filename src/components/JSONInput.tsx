b44a791edf206d4c08ca1a291047078219c07d2e
Thiago Arantes <thiagoarantes@gmail.com>
2023-05-07 01:02:07 -0400

    Initial input for JSONinput

--- src/components/JSONInput.tsx
+++ src/components/JSONInput.tsx
@@ -1,5 +1,16 @@
+import { styled } from "@/stitches";
+
+const DEFAULT_JSON =
+  "{\n\tname: 'Thiago',\n\tage: 40,\n\tprofession: 'Daddy'\n}";
+
+const Container = styled("textarea", {
+  width: "100%",
+  height: "100%",
+  padding: "$2",
+});
+
 const JSONInput = () => {
-  return <>JSON Input</>;
+  return <Container value={DEFAULT_JSON} />;
 };
 
 export default JSONInput;
